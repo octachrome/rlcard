b44bc4fa6d74621d295ae20d67e00527f7f81907
Chris Brown <chris@thebrown.net>
2023-03-04 17:17:20 +0000

    Comments

--- rlcard/games/coup/coup.py
+++ rlcard/games/coup/coup.py
@@ -12,6 +12,62 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+''' Implementation of the card game Coup.
+
+The rules can be found at https://boardgamegeek.com/boardgame/131357/coup.
+A flowchart clarifying the game play can be found at
+https://boardgamegeek.com/filepage/86105/action-resolution-order-flowchart.
+
+This implementation is intended for machine learning applications and is
+independent of any learning framework. All behaviour specific to RLCard
+is restricted to the files game.py and envs/coup.py.
+
+The original game contains elements of simultaneous play, but for simplicity
+(and compatibility with RLCard) the turn order is serialized and deterministic
+in this implementation. Particularly:
+
+- After a player declares an action or reaction which is linked to a role,
+  every other (living) player gets a chance to challenge the action, in turn
+  order. Whether or not each player will challenge is kept secret until all
+  players have responded. Ater this, the challenge is resolved. If the
+  challenge is correct (the player declaring the action does not reveal the
+  required role), then this player loses the influence, as usual. However, if
+  the challenge is incorrect, ALL players who chose to challenge must reveal
+  (and lose) an influence, in turn order. I feel this is in keeping with the
+  spirit of the original game.
+
+- There is one case where multiple players can choose to block an action:
+  foreign aid (blocked by any player with a duke). As with a challenge, each
+  player gets a chance to block (secretly). Once all players have decided,
+  if more than one player wants to block, one of them is selected at random
+  by the dealer. This is a significant deviation from the original rules, but
+  it is needed to avoid too much complexity. I doubt this will much affect an
+  agent's ability to learn a good strategy, since foreign aid is a minor action
+  in the game. If anything, agents will learn to block foreign aid slightly
+  more that they would otherwise, since there is a probability that its block
+  will have no effect (when the dealer chooses a different player to block.)
+
+Some other points where the rules sometimes vary from player to player:
+
+- When a player is challenged, he may choose to reveal a role that is different
+  from the one he claimed, i.e., intentionally losing the challenge as part of
+  a longer bluff. (In accordance with the flowchart.)
+
+- If a player assassinates, is challenged and loses the challenge, they do not
+  pay the 3 credits. If they win the challenge (or no one challenges), they pay
+  the 3 credits, regardless of whether the opponent blocks. (In accordance with
+  the flowchart.)
+
+- If a player assassinates and the opponent incorrectly challenges the
+  assassin, the opponent loses an influence. The opponent then still has the
+  chance to block with a contessa. (In accordance with the flowchart.)
+
+- If a player steals and the opponent dies (by incorrectly challenging the
+  captain), the stealing player gets no money. This is a convenient behaviour
+  given the implementation. (The correct behaviour in this case is not clear in
+  the official rules or flowchart.)
+
+'''
 import re
 
 from .dealer import CoupDealer as Dealer
@@ -23,10 +79,23 @@ class Coup:
     ''' Implementation of Coup that is agnostic to RLCard
     '''
     def __init__(self, num_players, np_random):
+        ''' Constructs a Coup game
+
+        Args:
+            num_players (int): number of players in the game
+            np_random (numpy.random.RandomState): used for randomized decisions
+        '''
         self.num_players = num_players
         self.np_random = np_random
 
     def init_game(self, dealer=None):
+        ''' Initializes a new game
+
+        Must be called at the start of every game.
+
+        Args:
+            dealer: If provided, overrides the default dealer (for testing)
+        '''
         self.dealer = dealer if dealer else Dealer(self.np_random)
         self.players = [Player(i) for i in range(self.num_players)]
         for player in self.players:
@@ -34,9 +103,22 @@ class Coup:
         self.state = Turn(self, 0)
 
     def player_to_act(self):
+        ''' Returns the id of the player who acts next
+
+        Returns:
+            (int): id of the player who must act in the current state
+
+        Note that in Coup, the next player to act depends on the previous
+        player's action.
+        '''
         return self.state.player_to_act()
 
     def play_action(self, action):
+        ''' Plays the given action
+
+        Args:
+            action (str): the action to play
+        '''
         self.state.play_action(action)
         if not self.is_game_over():
             player_id = self.state.player_to_act()
@@ -44,15 +126,54 @@ class Coup:
                 raise RuntimeError(f'Unexpected state: player to act is dead')
 
     def get_legal_actions(self):
+        ''' Returns a list of legal actions for the current player
+
+        Returns:
+            (list of string): list of action names that are legal in the current state
+        '''
         return self.state.get_legal_actions()
 
     def get_state(self):
+        ''' Returns a dictionary describing the current state of the game
+
+        See test_coup_game.py for examples of game states.
+
+        Returns:
+            (dict) that contains:
+                game (dict): a dict which may contain the following entries, depending on phase
+                    phase: current game phase (always present, one of constants.PHASES)
+                    whose_turn: id of the player who played the initial action in this turn
+                    player_to_act: id of the player who must now act
+                    action: the initial action played in this turn, if any
+                    target_player: the player targeted by the action, if any (assassinate, steal and coup only)
+                    blocked_with: the role used to block the action, if any
+                    blocking_player: the player who is blocking the action, if any
+                    drawn_roles: for the exchange action, the role cards drawn from the deck
+                    winning_player: id of the player who won, if the game is over
+                players (list of dict): a dict for each player, containing:
+                    cash (int): number of credits the player has
+                    hidden (list of str): hidden role cards of the player
+                    revealed (list of str): revealed role cards of the player
+                    trace (list of tuple): selected actions the player has taken which implicate the his roles
+        '''
         return {
             'players': [p.get_state() for p in self.players],
             'game': self.state.get_state()
         }
 
     def get_next_player(self, player_id):
+        ''' Returns the player whose turn will follow the given player
+
+        Args:
+            player_id (int): id of the player to query
+
+        Returns:
+            (int): id of the player who acts after player_id
+        
+        Used by Action subclasses to determine challenge order etc.
+
+        Dead players are skipped.
+        '''
         next_player_id = player_id
         while True:
             next_player_id = (next_player_id + 1) % self.num_players
@@ -63,12 +184,23 @@ class Coup:
         return next_player_id
 
     def end_turn(self):
+        ''' Finish the current turn and pass to the next player
+
+        Called by Action subclasses when the action is complete.
+        '''
         if not self.is_game_over():
             player_id = self.get_next_player(self.state.player_id)
             self.state = Turn(self, player_id)
 
     def reset_state(self, state):
-        ''' For debugging
+        ''' Resets the game to a given state
+
+        Args:
+            state (dict): a state dictionary such as returned by get_state
+
+        Currently only supports states representing the start of a player's turn.
+
+        Used for testing and debugging only.
         '''
         assert state['game']['phase'] == START_OF_TURN
         self.state = Turn(self, state['game']['whose_turn'])
@@ -78,9 +210,25 @@ class Coup:
             self.players[pid].revealed = p['revealed']
 
     def is_game_over(self):
+        ''' Returns whether the game is over
+
+        Returns:
+            (bool): True if the game is over
+        '''
         return type(self.state) == GameOver
 
     def reveal_role(self, player_id, role):
+        ''' Permanently reveals a player's influence
+
+        Args:
+            player_id (int): id of the player whose influence to reveal
+            role (str): the role to reveal
+
+        Called by Action subclasses when a player loses an influence.
+
+        The given role will be removed from the player's hidden influence and
+        added to the player's revealed influence.
+        '''
         player = self.players[player_id]
         player.hidden.remove(role)
         player.revealed.append(role)
@@ -89,60 +237,203 @@ class Coup:
             self.state = GameOver(live_players[0])
 
     def replace_role(self, player_id, role):
+        ''' Replaces a player's influence with another from the deck
+
+        Args:
+            player_id (int): id of the player who gets a new influence
+            role (str): the role to replace
+
+        Called by Action subclasses when a player must reveal a card but does
+        not lose one.
+        '''
         player = self.players[player_id]
         player.hidden.remove(role)
         player.hidden += self.dealer.deal_cards(1)
 
     def replace_all_roles(self, player_id, roles):
+        ''' Replaces all influence of a player
+
+        Args:
+            player_id (int): id of the player who gets new influence
+            roles (list of str): the new roles
+
+        Called at the end of an exchange action when the player has chosen the
+        roles to keep.
+        '''
         player = self.players[player_id]
         assert len(roles) == len(player.hidden)
         player.hidden = list(roles)
 
     def trace_claim(self, player_id, role):
+        ''' Records a role claim in a player's history
+
+        Args:
+            player_id (int): id of the player making the claim
+            role (str): the role claimed by the player
+
+        Called whenever a player claims to have a role, by taking an action or
+        blocking an opponent's action.
+
+        Role claims can be used by an agent to infer hidden information about a
+        player.
+        '''
         self.players[player_id].trace.append(('claim', role))
 
     def trace_reveal(self, player_id, role):
+        ''' Records a revealed role in a player's history
+
+        Args:
+            player_id (int): id of the player who revealed the role
+            role (str): the role revealed by the player
+
+        Called whenever a player reveals an influence due to a failed challenge,
+        assassination or coup.
+
+        Revealed roles can be used by an agent to learn how a player's prior
+        actions are correlated with the hidden roles that the player has.
+        '''
         self.players[player_id].trace.append(('reveal', role))
 
     def trace_exchange(self, player_id):
+        ''' Records an exchange in a player's history
+
+        Args:
+            player_id (int): id of the player who exchanged his roles
+
+        Called whenever a player successfully plays the the exchange action and
+        replaces his cards.
+
+        The player's previous roles and new roles are unknown to other players.
+
+        An agent can use this information to modify their model of a player's
+        hidden roles.
+        '''
         self.players[player_id].trace.append(('exchange',))
 
     def player_has_role(self, player_id, role):
+        ''' Returns whether a player has the given hidden role
+
+        Args:
+            player_id (int): id of the player to check
+            role (str): the role to check
+
+        Returns 
+        Used when determining the outcome of a challenge.
+        '''
         return role in self.players[player_id].hidden
 
     def is_alive(self, player_id):
+        ''' Returns whether a player is alive
+
+        Args:
+            player_id (int): id of the player to check
+
+        Returns:
+            (int): True of the player is alive
+        '''
         return len(self.players[player_id].hidden) > 0
 
     def get_influence(self, player_id):
+        ''' Returns a player's hidden roles
+
+        Args:
+            player_id (int): id of the player to check
+
+        Returns:
+            (list of str): the hidden roles of the player
+        '''
         return list(self.players[player_id].hidden)
 
     def add_cash(self, player_id, cash):
+        ''' Add some credits to a player's balance
+
+        Args:
+            player_id (int): id of the player to credit
+            cash (int): number of credits to add
+        '''
         self.players[player_id].cash += cash
 
     def deduct_cash(self, player_id, cash):
+        ''' Subtract some credits from a player's balance
+
+        Args:
+            player_id (int): id of the player to debit
+            cash (int): number of credits to subtract
+
+        Returns:
+            (int): the number of credits that was actually subtracted
+
+        Ensures that a player never has negative credit, which is important
+        when stealing from a player with 1 credit: in this case, the theif will
+        only gain 1 credit.
+        '''
         cash = min(cash, self.players[player_id].cash)
         self.players[player_id].cash -= cash
         return cash
 
-    def can_afford(self, player_id, cost):
-        return self.players[player_id].cash >= cost
+    def can_afford(self, player_id, price):
+        ''' Returns whether a player can afford a price
+
+        Args:
+            player_id (int): id of the player to check
+            price (int): price to check
+
+        Returns:
+            (bool): True if the player can afford the price
+
+        Used to check whether actions with a cost (coup and assassination) can
+        be afforded by the current player.
+        '''
+        return self.players[player_id].cash >= price
 
     def choose(self, items):
+        ''' Randomly choses one of the given items
+
+        Args:
+            items (list): items to choose from
+
+        Returns:
+            The chosen item
+
+        Used to choose which player gets to a block foreign aid attempt when
+        more than one player tries to block.
+        '''
         return self.dealer.choose(items)
 
 class GameOver:
+    ''' State representing the end of the game
+    '''
     def __init__(self, winning_player):
+        ''' Constructs a game over state
+
+        Args:
+            winning_player (int): id of the player who won the game
+        '''
         self.winning_player = winning_player
 
     def get_state(self):
+        ''' Returns a dictionary describing the game state
+
+        See Coup.get_state.
+        '''
         return {'phase': GAME_OVER, 'winning_player': self.winning_player}
 
     def get_legal_actions(self):
+        ''' Returns a list of legal actions for the current player
+
+        See Coup.get_legal_actions.
+        '''
         return []
 
     def player_to_act(self):
+        ''' Returns the id of the player who acts next
+
+        See Coup.player_to_act.
+        '''
         return None
 
+''' Regular expression describing all possible actions at the start of a player's turn
+'''
 ACTION_RE = re.compile(
     '(' + '|'.join(UNTARGETED_ACTIONS) + ')|' +
     '(' + '|'.join(TARGETED_ACTIONS) + r'):(\d+)$'
