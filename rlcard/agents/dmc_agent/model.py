63d0da3a0ac4ddb00374507697e36200d013031a
Chris Brown <chris@thebrown.net>
2023-03-04 15:24:42 +0000

    Added option to make DMC agent play a mixed strategy

--- rlcard/agents/dmc_agent/model.py
+++ rlcard/agents/dmc_agent/model.py
@@ -42,6 +42,11 @@ class DMCNet(nn.Module):
         values = self.fc_layers(x).flatten()
         return values
 
+def rewards_to_probs(rewards):
+    t = torch.tensor(rewards)
+    p = torch.special.softmax(t, 0)
+    return p.numpy()
+
 class DMCAgent:
     def __init__(
         self,
@@ -50,18 +55,23 @@ class DMCAgent:
         mlp_layers=[512,512,512,512,512],
         exp_epsilon=0.01,
         device="0",
+        if_probabilistic=False,
     ):
         self.use_raw = False
         self.device = 'cuda:'+device if device != "cpu" else "cpu"
         self.net = DMCNet(state_shape, action_shape, mlp_layers).to(self.device)
         self.exp_epsilon = exp_epsilon
         self.action_shape = action_shape
+        self.if_probabilistic = if_probabilistic
 
     def step(self, state):
         action_keys, values = self.predict(state)
 
         if self.exp_epsilon > 0 and np.random.rand() < self.exp_epsilon:
             action = np.random.choice(action_keys)
+        elif self.if_probabilistic:
+            probs = rewards_to_probs(values)
+            action = np.random.choice(action_keys, p=probs)
         else:
             action_idx = np.argmax(values)
             action = action_keys[action_idx]
@@ -128,7 +138,8 @@ class DMCModel:
         action_shape,
         mlp_layers=[512,512,512,512,512],
         exp_epsilon=0.01,
-        device=0
+        device=0,
+        if_probabilistic=False,
     ):
         self.agents = []
         for player_id in range(len(state_shape)):
@@ -138,6 +149,7 @@ class DMCModel:
                 mlp_layers,
                 exp_epsilon,
                 device,
+                if_probabilistic,
             )
             self.agents.append(agent)
 
